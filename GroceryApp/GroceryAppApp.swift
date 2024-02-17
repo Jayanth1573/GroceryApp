2c8888ffb9e1f323848bff25f53d8d8d8764fa3f
Jayanth <jayanthambaldhage15@gmail.com>
2024-02-17 15:51:59 +0530

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Jayanth Ambaldhage on 17/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
